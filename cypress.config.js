dd08f2608c26e1158b84d1f7024d35ab75e06900
bharati-yedave <bharatiyedave139@gmail.com>
2026-03-15 19:40:18 +0530

    report configuration changed4

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/reports",
-    overwrite: true,
+    overwrite: false,
     html: true,
     json: true,
   },
