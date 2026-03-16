3fa81f5ef1e00c18dbcfefacc8a8feeebf62138c
bharati-yedave <bharatiyedave139@gmail.com>
2026-03-16 09:43:57 +0530

    file added spec3.cy.js

---
+++ cypress/e2e/spec3.cy..js
@@ -0,0 +1,7 @@
+describe("Testing ORANGEHRM Scenario", () => {
+  it("Naigate to URL specs3.cy.js", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+  });
+});
